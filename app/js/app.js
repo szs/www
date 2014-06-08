d5a339c32c068d25517e33226bef5ef857b74ed2
Sherin Siew <sz.sherin@gmail.com>
2014-06-08 18:53:15 +0800

    Adds 'Almost Exactly' standalone page

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,49 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 // $(document).foundation();
 
-$(document).ready(function() {
+$(function() {
     $('#fullpage').fullpage({});
-});
+
+	$('.responsive').slick({
+		autoplay: true,
+		autoplaySpeed: 60000,
+		pauseOnHover: true,
+		dots: true,
+		infinite: true,
+		speed: 900,
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		responsive: [
+			{
+			  breakpoint: 1024,
+			  settings: {
+			    slidesToShow: 1,
+			    slidesToScroll: 1,
+			    infinite: true,
+			    dots: true
+			  }
+			},
+			{
+			  breakpoint: 600,
+			  settings: {
+			    slidesToShow: 1,
+			    slidesToScroll: 1
+			  }
+			},
+			{
+			  breakpoint: 480,
+			  settings: {
+			    slidesToShow: 1,
+			    slidesToScroll: 1
+			  }
+			}
+		]
+	});
+
+	// Super nasty - please figure out why this is set in the first place.
+	$('body').css('overflow-y','scroll');
+
+
+
+});	
+	
